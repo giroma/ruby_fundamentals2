8ebd55f54ece6d140ee3954acdb3d4242e3ccf3b
Argir Argirov <dj.giro@gmail.com>
2017-09-19 14:54:33 -0400

    DONE exercise 1 -5

---
+++ methods.rb
@@ -0,0 +1,42 @@
+# exercise 1
+def double(my_number)
+  return my_number * 2
+end
+
+# exercise 2
+def negative?(my_number)
+  if my_number < 0
+    return true
+  else
+    return false
+  end
+end
+
+# exercise 3
+def is_even?(num)
+  if num % 2 == 0
+    return true
+  end
+    return false
+end
+
+# exercise 4
+def long_string?(user_string)
+  if user_string.length < 8
+    return false
+  end
+    true
+end
+
+# exercise 5
+def greet_backwards(user_name)
+  return p "Hello #{user_name.reverse}#{user_name.reverse}, welcome"
+end
+
+
+
+# exercise 6
+
+# exercise 7
+
+# exercise 8
